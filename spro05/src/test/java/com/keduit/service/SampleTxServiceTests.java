1ae846adc7534d7e21f4107ffd90b862aacc56a3
emptysky2022 <emptysky2022@gmail.com>
2023-01-20 17:55:26 +0900

    reply update

---
+++ spro05/src/test/java/com/keduit/service/SampleTxServiceTests.java
@@ -0,0 +1,26 @@
+package com.keduit.service;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.test.context.ContextConfiguration;
+import org.springframework.test.context.junit4.SpringJUnit4ClassRunner;
+
+import lombok.Setter;
+import lombok.extern.log4j.Log4j;
+
+@RunWith(SpringJUnit4ClassRunner.class)
+@Log4j
+@ContextConfiguration({"file:src/main/webapp/WEB-INF/spring/root-context.xml"})
+public class SampleTxServiceTests {
+	
+	@Setter(onMethod_ = @Autowired)
+	private SampleTxService service;
+	
+	@Test
+	public void testLong() {
+		String str = "when an unknown printer took a galley of type and scrambled it to make a type specimen book. It has survived not only five centuries, but also the leap into electronic typesetting, remaining essentially unchanged. It was popularised in the 1960s with the release of Letraset sheets containing Lorem Ipsum passages, and more recently with desktop publishing software like Aldus PageMaker including versions of Lorem Ipsum.";
+		log.info(str.getBytes().length);
+		service.addData(str);	
+	}
+}
